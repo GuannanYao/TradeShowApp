9bcc647acdb37189196a56460aa895b1f2c48a36
Guannan <guanan_y@hotmail.com>
2015-09-05 02:54:50 +1000

    Initial Commit

---
+++ TradeShowTests/TradeShowTests.swift
@@ -0,0 +1,36 @@
+//
+//  TradeShowTests.swift
+//  TradeShowTests
+//
+//  Created by Guannan Yao on 5/09/2015.
+//  Copyright (c) 2015 University of Wollongong. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TradeShowTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
