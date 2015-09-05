5904ee3389daccc9c5619a2d051efa185c7a4441
Guannan <guanan_y@hotmail.com>
2015-09-05 18:18:55 +1000

    fix file

---
+++ TradeShow/MapViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MapViewController.swift
+//  TradeShow
+//
+//  Created by Guannan Yao on 5/09/2015.
+//  Copyright (c) 2015 University of Wollongong. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    @IBOutlet weak var menuButton:UIBarButtonItem!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        if self.revealViewController() != nil{
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TradeShow/NewsTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  NewsTableViewCell.swift
+//  TradeShow
+//
+//  Created by Guannan Yao on 5/09/2015.
+//  Copyright (c) 2015 University of Wollongong. All rights reserved.
+//
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var postImageView:UIImageView!
+    @IBOutlet weak var authorImageView:UIImageView!
+    @IBOutlet weak var postTitleLabel:UILabel!
+    @IBOutlet weak var authorLabel:UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
